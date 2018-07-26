8e566c2aed52b258bb95281e3d0134894b0189b1
Marta Gajowczyk <gajowczyk.marta@gmail.com>
2018-07-26 18:28:42 +0100

    Remove reduntant code

--- components/global/icon/components/icon/icon-component.jsx
+++ components/global/icon/components/icon/icon-component.jsx
@@ -1,9 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { themr } from 'react-css-themr';
-import cx from 'classnames';
-
-import styles from './icon-styles.scss';
 
 /**
  * @render react
@@ -11,20 +7,18 @@ import styles from './icon-styles.scss';
  * Represents an icon
  * @example <Icon icon={icon} />
  */
-const Icon = ({ icon, theme, className = '', onClick }) => {
-  const classNames = cx(className, theme.icon);
+const Icon = ({ icon, theme, onClick }) => {
   return (
-    <img className={classNames} src={icon} />
+    <img className={theme.icon} src={icon} onClick={onClick} />
   );
 };
 
 Icon.propTypes = {
   icon: PropTypes.string,
-  className: PropTypes.string,
   theme: PropTypes.shape({
     icon: PropTypes.string
   }),
   onClick: PropTypes.func
 };
 
-export default themr('Icon', styles)(Icon);
+export default Icon;
