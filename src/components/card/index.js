483f80458791512ef9fb39ee8d305d2284a2b8bf
j8seangel <jseangel@gmail.com>
2018-07-30 15:17:39 +0200

    Exporting component test

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -1,3 +1,48 @@
-import Card from '@bit/aabdaab.cw-components.global.card';
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import cx from 'classnames';
+import CheckInput from '../check-input'
 
-export default Card
+import styles from './card-styles.scss';
+
+/**
+ * Represents a card with title and description
+ */
+class Card extends PureComponent {
+  render() {
+    const { title, subtitle, children, theme } = this.props;
+    return (
+      <div className={cx(styles.card, theme.card)}>
+        <div className={styles.data}>
+          <CheckInput
+            label={'Subscribe'}
+            toggleFirst
+            theme={styles}
+            disabled={false}
+          />
+        </div>
+        <div className={styles.contentContainer}>
+          {title && <p className={cx(styles.title, theme.title)}>{title}</p>}
+          {subtitle && <p className={cx(styles.subtitle, theme.subtitle)}>{subtitle}</p>}
+        </div>
+      </div>
+    );
+  }
+}
+
+Card.propTypes = {
+  title: PropTypes.string,
+  subtitle: PropTypes.string,
+  children: PropTypes.node,
+  theme: PropTypes.shape({
+    card: PropTypes.string,
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+  }),
+};
+
+Card.defaultProps = {
+  theme: {}
+};
+
+export default Card;
