65683b98a580dabb950ac25c54fd11fab03e6482
Marta Gajowczyk <gajowczyk.marta@gmail.com>
2018-07-31 17:17:49 +0200

    Add default styling to icon, add cursor pointer

--- src/components/icon/icon.js
+++ src/components/icon/icon.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import cx from 'classnames';
+import styles from './icon-styles.scss';
 
 /**
  * Icon component for displaying an icon
  */
-const Icon = ({ icon, theme, onClick }) => (
-  <img className={theme.icon} src={icon} alt="icon" onClick={onClick} />
-);
+const Icon = ({ icon, theme, onClick }) => {
+  const classNames = cx(styles.icon, onClick ? styles.cursor : '', theme.icon);
+
+  return <img className={classNames} src={icon} alt="icon" onClick={onClick} />;
+};
 
 Icon.propTypes = {
   /**
