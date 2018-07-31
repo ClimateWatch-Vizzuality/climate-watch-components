98114d551684c6e5f57cc1d7024809be9e197008
j8seangel <jseangel@gmail.com>
2018-07-31 19:56:05 +0200

    Update exported components

--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
+export { default as Accordion } from './components/accordion';
 export { default as Card } from './components/card';
 export { default as CheckInput } from './components/check-input';
+export { default as Icon } from './components/icon';
