792a8cc5c166600197b4d72d5c5f0caf6fd0fa9d
Marta Gajowczyk <gajowczyk.marta@gmail.com>
2018-08-02 13:27:56 +0200

    Add bottom bar to index.js file

--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@ export { default as Accordion } from './components/accordion';
 export { default as Card } from './components/card';
 export { default as CheckInput } from './components/check-input';
 export { default as Icon } from './components/icon';
+export { default as BottomBar } from './components/bottom-bar';
