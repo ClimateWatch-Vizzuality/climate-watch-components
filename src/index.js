456511ffdc2e63c997588559685a1d3e6bf0af78
Marta Gajowczyk <gajowczyk.marta@gmail.com>
2018-08-02 12:49:20 +0200

    Export info button in index.js

--- src/index.js
+++ src/index.js
@@ -3,3 +3,4 @@ export { default as Card } from './components/card';
 export { default as CheckInput } from './components/check-input';
 export { default as Icon } from './components/icon';
 export { default as Button } from './components/button';
+export { default as InfoButton } from './components/info-button';
