59287be86da9a54aa6d13ed6d96d7edab6206498
j8seangel <jseangel@gmail.com>
2018-08-03 16:04:27 +0200

    Include input text component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 export { default as Accordion } from './components/accordion';
 export { default as Card } from './components/card';
 export { default as CheckInput } from './components/check-input';
+export { default as Input } from './components/input';
 export { default as Icon } from './components/icon';
 export { default as BottomBar } from './components/bottom-bar';
 export { default as Button } from './components/button';
