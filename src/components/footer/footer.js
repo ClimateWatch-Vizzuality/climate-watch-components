19adc072295224ec76fdcaaed7c05f3e35f13ade
Marta Gajowczyk <gajowczyk.marta@gmail.com>
2018-08-06 13:51:44 +0100

    Add footer component
    * Add footer component
    * Make small changes to other components like BottomBar and Contact

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -14,36 +14,59 @@ class Footer extends PureComponent {
       partners,
       includePartners,
       includeContact,
-      includeBottom
+      contactOptions,
+      includeBottom,
+      theme
     } = this.props;
-    const className = cx(styles.footer, styles.border);
+    const className = cx(
+      styles.footer,
+      theme.footer,
+      styles.border,
+      theme.border
+    );
     return (
       <footer className={className}>
         {
           includePartners && (
-          <div className={cx(styles.content, styles.nav)}>
+          <div
+            className={cx(
+                  styles.content,
+                  theme.content,
+                  styles.nav,
+                  theme.nav
+                )}
+          >
             <div>Partners</div>
             <div className="grid-column-item">
-              <div className={styles.contentWrapper}>
+              <div
+                className={cx(styles.contentWrapper, theme.contentWrapper)}
+              >
                 <div className="grid-column-item">
-                  <div className={styles.partnersContainer}>
+                  <div className={theme.partnersContainer}>
                     {partners.map(
                           partner => partner.img && (
                           <div
                             key={partner.img.alt}
-                            className={styles.logoContainer}
+                            className={cx(
+                                  styles.logoContainer,
+                                  theme.logoContainer
+                                )}
                           >
                             <a
                               className={cx(
                                     styles.logo,
-                                    styles[partner.img.customClass] ||
-                                      styles.defaultLogo
+                                    theme.logo,
+                                    styles[partner.img.customClass]
                                   )}
                               href={partner.link}
                               target="_blank"
                               rel="noopener noreferrer"
                             >
                               <img
+                                className={cx(
+                                      styles.defaultLogo,
+                                      theme.logo
+                                    )}
                                 src={partner.img.src}
                                 alt={partner.img.alt}
                               />
@@ -53,29 +76,71 @@ class Footer extends PureComponent {
                         )}
                   </div>
                 </div>
-                {includeContact && <Contact />}
+                {
+                      includeContact &&
+                        (
+                          <Contact
+                            contactTitle={contactOptions.contactTitle}
+                            contactMail={contactOptions.contactMail}
+                          />
+                        )
+                    }
               </div>
             </div>
           </div>
             )
         }
-        {includeBottom && <BottomBar className={styles.content} />}
+        {
+          includeBottom &&
+            <BottomBar className={cx(styles.content, theme.content)} />
+        }
       </footer>
     );
   }
 }
 
 Footer.propTypes = {
-  partners: PropTypes.array.isRequired,
+  /** Array of partners with link and logo img */
+  partners: PropTypes.arrayOf(
+    PropTypes.shape({
+      link: PropTypes.string,
+      img: PropTypes.shape({
+        src: PropTypes.string,
+        alt: PropTypes.string,
+        customClass: PropTypes.string
+      })
+    })
+  ).isRequired,
   includePartners: PropTypes.bool,
   includeContact: PropTypes.bool,
-  includeBottom: PropTypes.bool
+  contactOptions: PropTypes.shape({
+    contactTitle: PropTypes.string,
+    contactMail: PropTypes.string,
+    theme: PropTypes.shape({
+      contactContainer: PropTypes.string,
+      contact: PropTypes.string,
+      icon: PropTypes.string
+    })
+  }),
+  includeBottom: PropTypes.bool,
+  theme: PropTypes.shape({
+    footer: PropTypes.string,
+    border: PropTypes.string,
+    content: PropTypes.string,
+    nav: PropTypes.string,
+    contentWrapper: PropTypes.string,
+    partnersContainer: PropTypes.string,
+    logoContainer: PropTypes.string,
+    logo: PropTypes.string
+  })
 };
 
 Footer.defaultProps = {
   includePartners: true,
   includeContact: true,
-  includeBottom: true
+  contactOptions: {},
+  includeBottom: true,
+  theme: {}
 };
 
 export default Footer;
