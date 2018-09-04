0ef3d67f703d7c00d25dbb7c437068789901a2af
Marta Gajowczyk <gajowczyk.marta@gmail.com>
2018-09-04 12:49:25 +0100

    Fix passing props to customTooltip in ComposedChart

--- src/components/charts/chart-composed/chart-composed.js
+++ src/components/charts/chart-composed/chart-composed.js
@@ -117,7 +117,8 @@ class ChartComposed extends PureComponent {
               cursor={{ stroke: '#113750', strokeWidth: 2 }}
               filterNull={false}
               content={content =>
-                customTooltip && <customTooltip content={content} /> ||
+                customTooltip &&
+                  React.cloneElement(customTooltip, { content }) ||
                   (
                     <TooltipChart
                       content={content}
