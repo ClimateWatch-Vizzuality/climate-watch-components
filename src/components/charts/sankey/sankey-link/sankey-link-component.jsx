27682fa3551029a5b42afb2b2affd5e390a1a045
Alvaro Leal <alvaro.leal@vizzuality.com>
2018-10-22 18:59:35 +0200

    Add width to link when hovering

--- src/components/charts/sankey/sankey-link/sankey-link-component.jsx
+++ src/components/charts/sankey/sankey-link/sankey-link-component.jsx
@@ -1,22 +1,34 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import { PropTypes } from 'prop-types';
 import styles from './sankey-link-styles.scss';
 
-function SankeyLink({ sourceX, sourceY, sourceControlX, targetX, targetY, targetControlX, linkWidth }) {
-  const minLinkWidth = 2;
-  return (
-    <path
-      className={styles.link}
-      d={`
-        M${sourceX},${sourceY}
-        C${sourceControlX},${sourceY} ${targetControlX},${targetY} ${targetX},${targetY}
-      `}
-      fill="none"
-      stroke="#333"
-      strokeWidth={linkWidth < minLinkWidth ? minLinkWidth : linkWidth}
-      strokeOpacity="0.2"
-    />
-  );
+class SankeyLink extends PureComponent {
+  constructor() {
+    super();
+    this.minLinkWidth = 2;
+    this.state = { hover: false };
+  }
+
+  render() {
+    const { sourceX, sourceY, sourceControlX, targetX, targetY, targetControlX, linkWidth } = this.props;
+    const { hover } = this.state;
+    const updatedLinkWidth = linkWidth < this.minLinkWidth ? this.minLinkWidth : linkWidth;
+    return (
+      <path
+        className={styles.link}
+        d={`
+          M${sourceX},${sourceY}
+          C${sourceControlX},${sourceY} ${targetControlX},${targetY} ${targetX},${targetY}
+        `}
+        fill="none"
+        stroke="#333"
+        strokeWidth={hover ? updatedLinkWidth + 2 : updatedLinkWidth}
+        onMouseEnter={() => this.setState({ hover: true })}
+        onMouseLeave={() => this.setState({ hover: false })}
+        strokeOpacity="0.2"
+      />
+    );
+  }
 }
 
 SankeyLink.propTypes = {
